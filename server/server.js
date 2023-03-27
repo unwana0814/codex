83aba5f9b30c0130d8e031ee6e60cb0db1a6c8f9
Unwana <brettygeorge3@gmail.com>
2023-03-27 20:33:03 +0100

    comit t

--- server/server.js
+++ server/server.js
@@ -47,4 +47,5 @@ app.post('/', async(req, res) => {
     }
 })
 
-app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log('Server is running on port http://localhost:5000'));
